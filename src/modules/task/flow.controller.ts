fc538c54c6aade6c20326012e93cdc11eb4e410e
chenxiaofei <15210257331@163.com>
2022-04-06 10:00:41 +0800

    fix: 文案

--- src/modules/task/flow.controller.ts
+++ src/modules/task/flow.controller.ts
@@ -1,5 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Controller, Post, UseGuards, Body, Request, Get, Query, ParseIntPipe, UsePipes, ValidationPipe, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Request,
+  Get,
+  Query,
+  ParseIntPipe,
+  UsePipes,
+  ValidationPipe,
+  Param,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { TaskAddDTO } from './dto/task-add.dto';
 import { ApiTags } from '@nestjs/swagger';
@@ -8,43 +20,49 @@ import { FlowAddDTO } from './dto/flow-add.dto';
 import { FlowUpdateDTO } from './dto/flow-update.dto';
 import { FlowService } from './flow.service';
 
-@ApiTags('六层节点相关接口')
+@ApiTags('流程节点相关接口')
 @Controller('flow')
 export class FlowController {
-    constructor(
-        private readonly flowService: FlowService
-    ) { }
+  constructor(private readonly flowService: FlowService) {}
 
-    @Post('/add')
-    @UseGuards(AuthGuard('jwt'))
-    public async add(@Body() flowAddDTO: FlowAddDTO, @Request() request: any): Promise<any> {
-        return this.flowService.add(flowAddDTO, request);
-    }
+  @Post('/add')
+  @UseGuards(AuthGuard('jwt'))
+  public async add(
+    @Body() flowAddDTO: FlowAddDTO,
+    @Request() request: any,
+  ): Promise<any> {
+    return this.flowService.add(flowAddDTO, request);
+  }
 
-    @Post('/update')
-    @UseGuards(AuthGuard('jwt'))
-    public async update(@Body() flowUpdateDTO: FlowUpdateDTO): Promise<any> {
-        return this.flowService.update(flowUpdateDTO);
-    }
+  @Post('/update')
+  @UseGuards(AuthGuard('jwt'))
+  public async update(@Body() flowUpdateDTO: FlowUpdateDTO): Promise<any> {
+    return this.flowService.update(flowUpdateDTO);
+  }
 
-    @Get('/list')
-    @UseGuards(AuthGuard('jwt'))
-    public async list(@Query('keywords') keywords: string, @Request() request: any): Promise<any> {
-        return this.flowService.list(keywords,request);
-    }
+  @Get('/list')
+  @UseGuards(AuthGuard('jwt'))
+  public async list(
+    @Query('keywords') keywords: string,
+    @Request() request: any,
+  ): Promise<any> {
+    return this.flowService.list(keywords, request);
+  }
 
-    @Get('/all')
-    @UseGuards(AuthGuard('jwt'))
-    public async all(@Request() request: any): Promise<any> {
-        return this.flowService.all(request);
-    }
+  @Get('/all')
+  @UseGuards(AuthGuard('jwt'))
+  public async all(@Request() request: any): Promise<any> {
+    return this.flowService.all(request);
+  }
 
-    // 删除分组
-    @Get('/delete/:id')
-    @UseGuards(AuthGuard('jwt'))
-    @Transaction()
-    public async delete(@Param('id', new ParseIntPipe()) id: number, @TransactionManager() maneger: EntityManager,): Promise<any> {
-        return this.flowService.delete(id, maneger);
-    }
-
+  // 删除分组
+  @Get('/delete/:id')
+  @UseGuards(AuthGuard('jwt'))
+  @Transaction()
+  public async delete(
+    @Param('id', new ParseIntPipe()) id: number,
+    @TransactionManager() maneger: EntityManager,
+  ): Promise<any> {
+    return this.flowService.delete(id, maneger);
+  }
 }
